2fa917593db25a00c03ac19a252f15d87dd53e12
raerae <rheajane.embalzado@cit.edu>
2025-10-28 12:03:08 +0800

    Revert "Home Feed"
    This reverts commit c120f1d59f8705d87bea245a52326a0d1b8a27cb.

--- frontend/src/components/HeaderSidebarLayout.jsx
+++ frontend/src/components/HeaderSidebarLayout.jsx
@@ -2,27 +2,14 @@ import React from "react";
 import Header from "./Header/Header";
 import Sidebar from "./Sidebar/Sidebar";
 
-export default function HeaderSidebarLayout({ children }) {
+export default function Layout({ children }) {
   return (
-    <div className="w-full h-screen flex bg-gray-50">
-      
-      {/* Sidebar */}
-      <div className="fixed z-40">
-        <Sidebar />
-      </div>
-
-      {/* Main content with header */}
-      <div className="flex-1 ml-20 flex flex-col h-full">
-
-        <div className="fixed left-20 top-0 w-[calc(100%-4rem)] h-16 bg-white z-30">
-          <Header />
-        </div>
-
-        <main className="scroll-container mt-16 overflow-y-auto p-6">
-          {children}
-        </main>
-
-      </div>
-    </div>
+    <>
+      <Header />
+      <Sidebar />
+      <main className="pt-16 pr-16">
+        {children}
+      </main>
+    </>
   );
 }
