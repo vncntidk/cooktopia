39094d321fd33b5d75f71147f1393fca001e0ca1
raerae <rheajane.embalzado@cit.edu>
2025-10-28 12:05:28 +0800

    Reapply "Home Feed"
    This reverts commit 2fa917593db25a00c03ac19a252f15d87dd53e12.

--- frontend/src/components/HeaderSidebarLayout.jsx
+++ frontend/src/components/HeaderSidebarLayout.jsx
@@ -2,14 +2,27 @@ import React from "react";
 import Header from "./Header/Header";
 import Sidebar from "./Sidebar/Sidebar";
 
-export default function Layout({ children }) {
+export default function HeaderSidebarLayout({ children }) {
   return (
-    <>
-      <Header />
-      <Sidebar />
-      <main className="pt-16 pr-16">
-        {children}
-      </main>
-    </>
+    <div className="w-full h-screen flex bg-gray-50">
+      
+      {/* Sidebar */}
+      <div className="fixed z-40">
+        <Sidebar />
+      </div>
+
+      {/* Main content with header */}
+      <div className="flex-1 ml-20 flex flex-col h-full">
+
+        <div className="fixed left-20 top-0 w-[calc(100%-4rem)] h-16 bg-white z-30">
+          <Header />
+        </div>
+
+        <main className="scroll-container mt-16 overflow-y-auto p-6">
+          {children}
+        </main>
+
+      </div>
+    </div>
   );
 }
